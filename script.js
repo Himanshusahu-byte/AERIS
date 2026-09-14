f95f18d4aa6aa6253b30ca78a5e1a49697c64cc4
Himanshhusahu-byte <Himanshusahu5116@gmail.com>
2026-09-14 15:21:43 +0530

    Connect frontend to live backend

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
        CONFIGURATION
     ===================================================== */
 
-    const API_BASE = "http://127.0.0.1:8000";
+    const API_BASE = " https://aeris-1-8wfy.onrender.com";
 
 
     /* =====================================================
